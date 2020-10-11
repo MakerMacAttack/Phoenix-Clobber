d4c317eae5ef6916e415a3b87f86e3619b02c16c
Mac <tmcpbmay@gmail.com>
2020-10-11 11:19:51 -0400

    add axios call and sort return

--- clobber/src/Components/Leaderboard.jsx
+++ clobber/src/Components/Leaderboard.jsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
 export default function Leaderboard() {
-  return <h1>Leaderboard</h1>
+  const [leaders, setLeaders] = useState([]);
+
+  async function getLeaders() {
+    const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/leaderboard`;
+    const resp = await axios.get(airtableURL, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+      },
+    });
+    const unsortedLeaders = resp.data.records
+    unsortedLeaders.sort((a, b) => {
+      if (a.createdTime > b.createdTime) {
+        return 1
+      } else if (a.createdTime < b.createdTime) {
+        return -1
+      }
+      return 0
+    })
+    // unsortedLeaders.sort((a, b) => {
+    //   return a.fields.diff_int - b.fields.diff_int
+    // })
+    // unsortedLeaders.sort((a, b) => {
+    //   return a.fields.turns - b.fields.turns
+    // })
+    setLeaders(unsortedLeaders)
+  }
+
+  getLeaders()
+
+  // useEffect(() => {
+  //   getJokes();
+  // }, [fetchReviews]);
+
+  return <h1>Leaderboard</h1>;
 }
